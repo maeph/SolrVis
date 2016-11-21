84ae207be60670f8fa2ad976a550eab172ebf341
Zbyszko Papierski <zbyszko.papierski@gmail.com>
2016-11-21 20:45:19 +1100

    #1: initial files - WIP

---
+++ js/main.js
@@ -0,0 +1,100 @@
+import SongLikesClient from "./solr_client";
+
+
+document.addEventListener('DOMContentLoaded', () => {
+
+        const cytoscape = require('cytoscape');
+        const cy = window.cy = cytoscape({
+            container: document.getElementById('cy'),
+            style: [
+                {
+                    selector: 'node',
+                    style: {
+                        label: 'data(name)',
+                        'background-color': 'data(color)'
+                    }
+                }, {
+                    selector: 'edge',
+                    style: {
+                        'curve-style': 'bezier',
+                        'width': 3,
+                        'target-arrow-shape': 'triangle',
+                        'target-arrow-fill': 'filled'
+                    }
+                }
+
+            ],
+            layout: {
+                name: 'circle'
+            }
+
+        });
+        document.getElementById('new-graph-button').addEventListener('click', requestNewGraph);
+    }
+);
+
+
+function requestNewGraph() {
+    let expr = document.getElementById('expr').value;
+    let path = document.getElementById('path').value;
+    let client = new SongLikesClient(path);
+    console.log(path);
+
+    cy.remove(cy.elements("node"));
+    client.getGraphData(expr,
+        (response) => response.then(
+            (result) => {
+                const nodes = result['result-set'].docs.filter(node => typeof node.node != 'undefined');
+
+                var nodesData = nodes.map(node => {
+                    return {
+                        data: {
+                            id: node.node.indexOf("/") !== -1 ? node.node : node.collection + "/" + node.node,
+                            name: node.node,
+                            color: strToRGB(node.field)
+                        }
+                    };
+                });
+                var edgesData = nodes.map(node => {
+                    let nodeData = [];
+                    if (typeof node.ancestors != 'undefined')
+                        node.ancestors.forEach(ancestor => {
+                            nodeData.push({
+                                    data: {
+                                        source: ancestor.indexOf("/") !== -1 ? ancestor: node.collection + "/" + ancestor,
+                                        target: node.node.indexOf("/") !== -1 ? node.node : node.collection + "/" + node.node,
+                                        directed: true
+                                    }
+                                }
+                            )
+                        });
+                    return nodeData;
+
+                });
+
+                nodesData.forEach(node => cy.add(node));
+                edgesData.forEach(nodeData => nodeData.forEach(edge => cy.add(edge)));
+                cy.layout({
+                    name: 'breadthfirst'
+                });
+                cy.fit();
+            })
+    );
+}
+
+function hashCode(str) {
+    var hash = 0;
+    for (var i = 0; i < str.length; i++) {
+        hash = str.charCodeAt(i) + ((hash << 5) - hash);
+    }
+    return hash;
+}
+
+function strToRGB(str){
+    var i = hashCode(str);
+    var c = (i & 0x00FFFFFF)
+        .toString(16)
+        .toUpperCase();
+
+    return "#" + "00000".substring(0, 6 - c.length) + c;
+}
---
+++ js/solr_client.js
@@ -0,0 +1,22 @@
+import restClient, { SETTINGS } from '../node_modules/es6-rest-client/dist/client.es6';
+
+export default class StreamingExprClient {
+    constructor(url) {
+
+        this.url = url;
+
+    }
+
+    getGraphData(expr, callback) {
+        restClient[SETTINGS] ({
+            method: 'POST',
+            baseURI: this.url,
+            params: {expr: expr}
+        });
+
+        restClient().then((response) => {
+            callback(response.json())
+        })
+
+    }
+}
